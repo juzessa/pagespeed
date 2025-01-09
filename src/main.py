857c6d2298ff0ed7c1765699795d1fbeed4b80ce
Юлия Самсонова <samsonovajulias@gmail.com>
2025-01-09 18:18:33 +0300

    Correct pep

--- src/main.py
+++ src/main.py
@@ -1,8 +1,11 @@
-from typing import Union
-from fastapi.responses import HTMLResponse
+from datetime import datetime
+from typing import Optional
+
+from fastapi import FastAPI, Form, Request
+from fastapi.responses import HTMLResponse, RedirectResponse
 from fastapi.staticfiles import StaticFiles
-from fastapi import FastAPI, Request
 from fastapi.templating import Jinja2Templates
+from pydantic import BaseModel, HttpUrl
 
 app = FastAPI()
 
@@ -11,11 +14,37 @@ app.mount("/static", StaticFiles(directory="static"), name="static")
 templates = Jinja2Templates(directory="templates")
 
 
+class Site(BaseModel):
+    id: int
+    url: HttpUrl
+    date: Optional[datetime] = datetime.now()
+
+
+sites = {}
+
+
 @app.get("/", response_class=HTMLResponse)
 def read_root(request: Request):
-     return templates.TemplateResponse(request=request, name="index.html")
+    return templates.TemplateResponse(request=request, name="index.html")
 
 
-@app.get("/urls")
+@app.get("/urls", response_class=HTMLResponse)
 def read_urls(request: Request):
     return templates.TemplateResponse(request=request, name="urls.html")
+
+
+@app.post("/urls", response_class=RedirectResponse)
+def post_url(request: Request, url: HttpUrl = Form(...)):
+    if url in sites:
+        pass
+    site = Site(id=len(sites) + 1, url=url)
+    sites[site.id] = site
+    url_id = site.id
+    return RedirectResponse(f"/urls/{url_id}", status_code=303)
+
+
+@app.get("/urls/{url_id}", response_class=HTMLResponse)
+def get_url(request: Request, url_id: int):
+    site = sites.get(url_id)
+    return templates.TemplateResponse(
+        "urls2.html", {"request": request, "site": site})
