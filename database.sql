4e6c427926a6396ff94943d8f40c2f195cdffe7f
Юлия Самсонова <samsonovajulias@gmail.com>
2025-01-25 14:09:14 +0300

    Format html

--- database.sql
+++ database.sql
@@ -4,3 +4,15 @@ create table urls
 	name varchar,
 	created_at default current_timestamp
 );
+
+create table url_checks
+(
+	id serial primary key,
+	url_id int not null,
+	status_code int,
+	h1 varchar,
+	title varchar,
+	description varchar,
+	created_at timestamp default current_timestamp,
+	foreign key(url_id) references urls(id)
+);
